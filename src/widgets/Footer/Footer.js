b178298d349ce15ade61aab1c2b403bf23bac8d2
Yana Morozova <yanabonne@yandex.ru>
2023-02-25 21:42:52 +0300

    feat: add personal contacts to Footer

--- src/widgets/Footer/Footer.js
+++ src/widgets/Footer/Footer.js
@@ -1,13 +1,28 @@
 import "./Footer.css";
 
-function Footer() {  
+function Footer() {
   return (
     <footer className="footer">
-        <div className="footer__icons">
-            <button type='button' className="footer__icon footer__icon_github"></button>
-            <button type='button' className="footer__icon footer__icon_gmail"></button>
-            <button type='button' className="footer__icon footer__icon_tg"></button>
-        </div>
+      <div className="footer__icons">
+        <a href="https://github.com/Yanabonne" target="_blank" rel="noreferrer">
+          <button
+            type="button"
+            className="footer__icon footer__icon_github"
+          ></button>
+        </a>
+        <a href="mailto:yanamormmm@gmail.com" target="_blank" rel="noreferrer">
+          <button
+            type="button"
+            className="footer__icon footer__icon_gmail"
+          ></button>
+        </a>
+        <a href="https://t.me/chek_ess" target="_blank" rel="noreferrer">
+          <button
+            type="button"
+            className="footer__icon footer__icon_tg"
+          ></button>
+        </a>
+      </div>
       <p className="footer__author">Яна Морозова, 2023</p>
     </footer>
   );
