9b189f74fafe2f51e2c2f4a01cc8e527a3f2c3e5
Yana Morozova <yanabonne@yandex.ru>
2023-02-26 13:52:04 +0300

    feat: change random prediction style and remove header logo highlight

--- src/widgets/RandomGoOut/RandomGoOut.js
+++ src/widgets/RandomGoOut/RandomGoOut.js
@@ -3,25 +3,24 @@ import "./RandomGoOut.css";
 import { randomAnswers } from "../../shared/constants/constants";
 
 function RandomGoOut() {
-  const [isRaindropSeen, setIsRaindropSeen] = React.useState(false);
+  const [isTextSeen, setIsTextSeen] = React.useState(false);
   const [buttonText, setButtonText] = React.useState('Выходить ли мне на улицу?');
 
   function getRandomAnswer() {
     setButtonText(randomAnswers[Math.floor(Math.random() * randomAnswers.length)]);
-    setIsRaindropSeen(false);
+    setIsTextSeen(false);
   }
 
   function onButtonClick(e) {
-    if (!e.target.classList.contains("random__raindrop")) {
-        setButtonText('');
-        setIsRaindropSeen(true);
-        setTimeout(getRandomAnswer, 2000);
+    if (!e.target.classList.contains("random__no-text")) {
+        setIsTextSeen(true);
+        setTimeout(getRandomAnswer, 1000);
     }
   }
 
   return (
     <button
-      className={`${isRaindropSeen ? "random random_raindrop" : "random"}`}
+      className={`${isTextSeen ? "random random_no-text" : "random"}`}
       onClick={onButtonClick}
     >
       {buttonText}
